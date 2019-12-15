03b81af4eddf8afa892ffef818ce7bdbaf970bd0
Liam Pimlott <liam.pimlott@boldcommerce.com>
2019-12-15 14:29:12 -0600

    use agent config struct instead of file

--- main.go
+++ main.go
@@ -4,35 +4,37 @@ import (
 	"fmt"
 	"os"
 	"regexp"
-	// "strings"
-	// "time"
 
 	"github.com/turnage/graw"
 	"github.com/turnage/graw/reddit"
 )
 
-const (
-	envSubreddit  = "SWEARBOT_SUBREDDIT"
-	envAgentFile  = "SWEARBOT_AGENT_FILE"
-	envSwearRegex = "SWEARBOT_REGEX"
-)
-
 var (
-	subreddit  string
-	agentFile  string
-	swearRegex string
-	re         *regexp.Regexp
+	subreddit      string
+	swearRegex     string
+	swearBotConfig reddit.BotConfig
+	re             *regexp.Regexp
 )
 
 func init() {
-	subreddit = os.Getenv(envSubreddit)
-	agentFile = os.Getenv(envAgentFile)
-	swearRegex = os.Getenv(envSwearRegex)
+	subreddit = os.Getenv("SWEARBOT_SUBREDDIT")
+
+	swearRegex = os.Getenv("SWEARBOT_AGENT_FILE")
 	re = regexp.MustCompile(swearRegex)
+
+	swearBotConfig = reddit.BotConfig{
+		Agent: os.Getenv("SWEARBOT_USER_AGENT"),
+		App: reddit.App{
+			ID:       os.Getenv("SWEARBOT_APP_ID"),
+			Secret:   os.Getenv("SWEARBOT_APP_SECRET"),
+			Username: os.Getenv("SWEARBOT_APP_USERNAME"),
+			Password: os.Getenv("SWEARBOT_APP_PASSWORD"),
+		},
+	}
 }
 
 func main() {
-	if bot, err := reddit.NewBotFromAgentFile(agentFile, 0); err != nil {
+	if bot, err := reddit.NewBot(swearBotConfig); err != nil {
 		fmt.Println("Failed to create bot handle: ", err)
 	} else {
 		cfg := graw.Config{
