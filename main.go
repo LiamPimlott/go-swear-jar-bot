13cdea9a98634674ac33820ff90f277fb67cb395
Liam Pimlott <liam.pimlott@boldcommerce.com>
2019-12-16 21:05:48 -0600

    add bolt

--- main.go
+++ main.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"regexp"
 
+	"github.com/boltdb/bolt"
 	"github.com/turnage/graw"
 	"github.com/turnage/graw/reddit"
 )
 
+const usersKey = "users"
+
 var (
 	subreddit      string
-	swearRegex     string
-	swearBotConfig reddit.BotConfig
 	re             *regexp.Regexp
+	swearBotConfig reddit.BotConfig
+	grawConfig     graw.Config
 )
 
 func init() {
 	subreddit = os.Getenv("SWEARBOT_SUBREDDIT")
-
-	swearRegex = os.Getenv("SWEARBOT_AGENT_FILE")
-	re = regexp.MustCompile(swearRegex)
+	re = regexp.MustCompile(os.Getenv("SWEARBOT_REGEX"))
 
 	swearBotConfig = reddit.BotConfig{
 		Agent: os.Getenv("SWEARBOT_USER_AGENT"),
@@ -31,21 +33,44 @@ func init() {
 			Password: os.Getenv("SWEARBOT_APP_PASSWORD"),
 		},
 	}
+
+	grawConfig = graw.Config{
+		Subreddits:        []string{subreddit},
+		SubredditComments: []string{subreddit},
+	}
 }
 
 func main() {
-	if bot, err := reddit.NewBot(swearBotConfig); err != nil {
-		fmt.Println("Failed to create bot handle: ", err)
-	} else {
-		cfg := graw.Config{
-			Subreddits:        []string{subreddit},
-			SubredditComments: []string{subreddit},
-		}
-		handler := &swearBot{bot: bot}
-		if _, wait, err := graw.Run(handler, bot, cfg); err != nil {
-			fmt.Println("Failed to start graw run: ", err)
-		} else {
-			fmt.Println("graw run failed: ", wait())
-		}
+	// Open the swearbot.db data file in your current directory.
+	// It will be created if it doesn't exist.
+	db, err := bolt.Open("swearbot.db", 0600, nil)
+	if err != nil {
+		log.Fatalf("Failed to open db: %s", err)
 	}
+	defer db.Close()
+
+	// Init swearbot.db buckets if they dont exist
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(usersKey))
+		if err != nil {
+			return fmt.Errorf("Failed to create bucket \"%s\": %s", usersKey, err)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("Failed to initialize db: %s", err)
+	}
+
+	bot, err := reddit.NewBot(swearBotConfig)
+	if err != nil {
+		log.Fatalf("Failed to create bot handle: %s", err)
+	}
+
+	handler := &swearBot{bot: bot, db: db, regex: re}
+	_, wait, err := graw.Run(handler, bot, grawConfig)
+	if err != nil {
+		log.Fatalf("Failed to start graw run: %s", err)
+	}
+
+	log.Fatal("graw run failed: ", wait())
 }
--- swearbot.go
+++ swearbot.go
@@ -2,23 +2,79 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"regexp"
+	"strconv"
 
+	"github.com/boltdb/bolt"
 	"github.com/turnage/graw/reddit"
 )
 
 type swearBot struct {
-	bot reddit.Bot
+	bot   reddit.Bot
+	db    *bolt.DB
+	regex *regexp.Regexp
 }
 
-func (r *swearBot) Post(p *reddit.Post) error {
-	bodySwears := re.FindAllString(p.SelfText, -1)
-	titleSwears := re.FindAllString(p.Title, -1)
-	fmt.Printf("*** NEW POST *** id: %s  author: %s title_swears: %+v body_swears: %+v created_at: %d\n", p.Name, p.Author, titleSwears, bodySwears, p.CreatedUTC)
+func (sb *swearBot) Post(p *reddit.Post) error {
+	bodySwears := sb.regex.FindAllString(p.SelfText, -1)
+	titleSwears := sb.regex.FindAllString(p.Title, -1)
+	log.Printf("*** NEW POST *** id: %s  author: %s title_swears: %+v body_swears: %+v created_at: %d\n", p.Name, p.Author, titleSwears, bodySwears, p.CreatedUTC)
+
+	total, err := sb.updateSwearCount(len(bodySwears)+len(titleSwears), p.Author)
+	if err != nil {
+		return fmt.Errorf("Failed to update swear count after post (%s): %s", err, p.Name)
+	}
+	log.Printf("*** USER BUCKET UPDATED *** key: %s  value: %s post: %s\n", p.Author, total, p.Name)
+
 	return nil
 }
 
-func (r *swearBot) Comment(p *reddit.Comment) error {
-	bodySwears := re.FindAllString(p.Body, -1)
-	fmt.Printf("*** NEW COMMENT *** id: %s author: %s swears: %+v created_at: %d parent_id: %s\n", p.Name, p.Author, bodySwears, p.CreatedUTC, p.ParentID)
+func (sb *swearBot) Comment(c *reddit.Comment) error {
+	bodySwears := sb.regex.FindAllString(c.Body, -1)
+	fmt.Printf("*** NEW COMMENT *** id: %s author: %s total_swears: %v created_at: %d parent_id: %s\n", c.Name, c.Author, len(bodySwears), c.CreatedUTC, c.ParentID)
+
+	total, err := sb.updateSwearCount(len(bodySwears), c.Author)
+	if err != nil {
+		return fmt.Errorf("Failed to update swear count after comment (%s): %s", err, c.Name)
+	}
+	log.Printf("*** USER BUCKET UPDATED *** key: %s  value: %s post: %s\n", c.Author, total, c.Name)
+
 	return nil
+}
+
+func (sb *swearBot) updateSwearCount(newSwears int, user string) (string, error) {
+	var newTotal []byte
+
+	err := sb.db.Update(func(tx *bolt.Tx) error {
+		var err error
+		var currTotal int
+
+		bucket := tx.Bucket([]byte(usersKey))
+		key := []byte(user)
+		currVal := string(bucket.Get(key))
+
+		if currVal == "" {
+			currTotal = 0
+		} else {
+			currTotal, err = strconv.Atoi(currVal)
+			if err != nil {
+				return fmt.Errorf("Failed convert string \"%s\"to int: %s", err, currVal)
+			}
+		}
+
+		newTotal = []byte(strconv.Itoa(currTotal + newSwears))
+
+		err = bucket.Put([]byte(key), newTotal)
+		if err != nil {
+			return fmt.Errorf("Failed to put value (%s) to bucket (%s): %s", newTotal, usersKey, err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to execute batch: %s", err)
+	}
+
+	return string(newTotal), nil
 }
